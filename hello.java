b5fcf66fd0ed62b36a0f1fc33299db217ce432ca
SSD17921 <100840213+SSD17921@users.noreply.github.com>
2024-05-29 12:28:46 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,6 @@
 public class HelloWorld{
   public static void main(String[] args){
-  System.out.println("Hello Git");
+  System.out.println("Hello Git develper 1");
+    System.out.println("Hello Git develper 2");
   }
 }
